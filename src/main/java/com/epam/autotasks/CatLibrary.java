87255a4a53ae2d3f64a64dc606da974b197d5331
Sofia Krylova <sofia_krylova@epam.com>
2022-12-26 14:49:00 +0100

    Initial

---
+++ src/main/java/com/epam/autotasks/CatLibrary.java
@@ -0,0 +1,31 @@
+package com.epam.autotasks;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class CatLibrary {
+
+    public static final String EMPTY_STRING = "";
+
+    public Map<String, Cat> mapCatsByName(List<Cat> cats) {
+        return null;
+    }
+
+    public Map<Cat.Breed, Set<Cat>> mapCatsByBreed(List<Cat> cats) {
+        return null;
+    }
+
+    public Map<Cat.Breed, String> mapCatNamesByBreed(List<Cat> cats) {
+        return null;
+    }
+
+    public Map<Cat.Breed, Double> mapAverageResultByBreed(List<Cat> cats) {
+        return null;
+    }
+
+    public SortedSet<Cat> getOrderedCatsByContestResults(List<Cat> cats) {
+        return null;
+    }
+}
